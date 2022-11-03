ee84366c1337a9d3dd1121fb046d98f16f52763c
Manuel Vázquez <manvazmar3@alum.us.es>
2022-11-03 17:30:30 +0100

    entrega examen

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
@@ -1,17 +1,34 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoom {
-    Integer id;
-    String name;
-    double size;
-    boolean secure;
-    @Transient
+@Entity
+@Table(name="Recovery")
+public class RecoveryRoom extends BaseEntity{
+    @NotEmpty
+    @Size(min=3, max=50)
+    private String name;
+    @NotEmpty
+    @PositiveOrZero
+    private double size;
+    @NotNull
+    private boolean secure;
+    @ManyToOne
+    @JoinColumn(name="type")
     RecoveryRoomType roomType;
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomController.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomController.java
@@ -1,5 +1,46 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/recoveryroom")
 public class RecoveryRoomController {
+	private static final String VIEW_RECOVERYROOMS_CREATE_FORM =
+			"recoveryroom/createOrUpdateRecoveryRoomForm";
+	private static final String WELCOME = "welcome";
+	private RecoveryRoomService pservice;
+	
+	@Autowired
+	public RecoveryRoomController(RecoveryRoomService service) {
+		this.pservice = service;
+	}
+	
+	@GetMapping("/create")
+	public String initRecoveryRoom(ModelMap map) {
+		map.addAttribute("recoveryRoom", new RecoveryRoom());
+		map.addAttribute("types", pservice.getAllRecoveryRoomTypes());
+		return VIEW_RECOVERYROOMS_CREATE_FORM;
+	}
+	
+	@PostMapping(path="/create")
+	public String createRecoveryRoom(@Valid RecoveryRoom room, BindingResult br, ModelMap map) {
+		if(!br.hasErrors()) {
+			pservice.save(room);
+			map.addAttribute("messagge", "RecoveryRoom successfully save");
+			return WELCOME;
+		} else {
+			map.addAttribute("recoveryRoom", room);
+			map.addAttribute("types", pservice.getAllRecoveryRoomTypes());
+		}
+		return VIEW_RECOVERYROOMS_CREATE_FORM;
+	}
     
 }
